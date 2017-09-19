ecc12126c369ce7634c085dafac19b1d5a9bdab0
Alexander <sanchez.reyes2501@gmail.com>
2017-09-19 14:39:50 -0500

    step 4

--- class_rpg.py
+++ class_rpg.py
@@ -7,6 +7,11 @@ class Hero:
         print("You do {} damage to the goblin.".format(hero_power))
         if goblin.health <= 0:
             print("The goblin is dead.")
+    def alive(self):
+        while self.health >0:
+            return True
+        if self.health < 0:
+            return False
 
 class Goblin:
     def __init__(self, health, power):
@@ -17,3 +22,8 @@ class Goblin:
         print("The goblin does {} damage to you.".format(self.power))
         if hero.health <= 0:
             print("You are dead.")
+    def alive(self):
+        while self.health > 0:
+            return True
+        if self.health < 0:
+            return False
