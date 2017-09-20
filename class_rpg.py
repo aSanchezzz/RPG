0d7a279a787fda0555e618fefcfc6850948dbfd0
Alexander <sanchez.reyes2501@gmail.com>
2017-09-20 09:07:35 -0500

    step 7 (Revised)

--- class_rpg.py
+++ class_rpg.py
@@ -1,42 +1,7 @@
-class Hero:
-    def __init__(self, health, power):
-        self.health = 10
-        self.power = 5
-    def attack(self, goblin):
-        goblin.health -= self.power
-        print("You do {} damage to the goblin.".format(self.power))
-        if goblin.health <= 0:
-            print("The goblin is dead.")
-    def alive(self):
-        if self.health > 0:
-            return True
-        else:
-            return False
-    def print_status(self):
-        while self.health > 0:
-            print("You have {} health and {} power.".format(self.health, self.power))
-
-class Goblin:
-    def __init__(self, health, power):
-        self.health = 6
-        self.power = 2
-    def attack(self, hero):
-        hero.health -= self.power
-        print("The goblin does {} damage to you.".format(self.power))
-        if hero.health <= 0:
-            print("You are dead.")
-    def alive(self):
-        if self.health > 0:
-            return True
-        else:
-            return False
-    def print_status(self):
-        while self.health > 0:
-            print("The goblin has {} health and {} power.".format(self.health, self.power))
-
 #generic character class for both hero and goblin to inherit
 class Character:
-    def __init__(self, health, power):
+    def __init__(self, name):
+        self.name = name
         self.health = health
         self.power = power
     def alive(self):
@@ -44,3 +9,16 @@ class Character:
             return True
         else:
             return False
+
+#new hero and goblin classes inheriting 'Character' class
+class Hero(Character):
+    def __init__(self):
+        self.name = 'hero'
+        self.health = 10
+        self.power = 5
+
+class Goblin(Character):
+    def __init__(self):
+        self.name = 'goblin'
+        self.health = 6
+        self.power = 4
